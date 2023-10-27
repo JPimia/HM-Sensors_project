262fb51ac51665681d09ec05597d256ada4b4afc
masisoranummi <masi.soranummi@tuni.fi>
2023-10-27 12:04:55 +0300

    add popup-component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,34 @@
-import { useState } from 'react';
-import { DatastreamContent, SensorsList} from './components/components';
+import { useState } from "react";
+import { DatastreamContent, SensorsList } from "./components/components";
+import PopupComponent from "./components/PopupComponent";
 
 function App() {
-  const [selectedDatastream, setSelectedDatastream] = useState(null);
+    const [selectedDatastream, setSelectedDatastream] = useState(null);
 
-	return (
-		<div style={{display: 'flex'}}>
-			<div style={{width: '25%', height: '100%', overflowY: 'scroll'}}>
-				<SensorsList setSelectedDatastream={setSelectedDatastream} />
-			</div>
-			
-			<div style={{width: '75%'}}>
-				{selectedDatastream && (
-					<DatastreamContent datastream={selectedDatastream} />
-				)}
-			</div>
-		</div>
-	);
+    return (
+        <>
+            <PopupComponent />
+            <div style={{ display: "flex" }}>
+                <div
+                    style={{
+                        width: "25%",
+                        height: "100%",
+                        overflowY: "scroll",
+                    }}
+                >
+                    <SensorsList
+                        setSelectedDatastream={setSelectedDatastream}
+                    />
+                </div>
+
+                <div style={{ width: "75%" }}>
+                    {selectedDatastream && (
+                        <DatastreamContent datastream={selectedDatastream} />
+                    )}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
