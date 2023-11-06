a6d24ae837017511687648877ea1c70bf254d8a4
Koydenjatke <97977463+Koydenjatke@users.noreply.github.com>
2023-11-06 15:19:37 +0200

    Fix Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { DatastreamContent, SensorsList } from './components/components';
 import PopupComponent from './components/PopupComponent';
 import ChartGraphComparison from './components/chartGraphComparisons';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import SensorPage from './components/sensorPage';
 
 function App() {
@@ -29,51 +29,50 @@ function App() {
 				</div>
 			</div>
 
-			<Route
-				exact
-				path="/"
-				render={ () => (
-					<div style={ { display: 'flex' } }>
-						<div
-							style={ {
-								width: '25%',
-								height: '100%',
-								overflowY: 'scroll',
-							} }
-						>
-							<SensorsList
-								setSelectedDatastream={ setSelectedDatastream }
-								setDatastreamComparisonList={
-									setDatastreamComparisonList
-								}
-								selectedSensors={ selectedSensors }
-								setSelectedSensors={ setSelectedSensors }
-							/>
-						</div>
-						<div style={ { width: '75%' } }>
-							{selectedDatastream && (
-								<DatastreamContent
-									datastream={ selectedDatastream }
+			<Routes>
+				<Route
+					path="/"
+					element={ (
+						<div style={ { display: 'flex' } }>
+							<div
+								style={ {
+									width: '25%',
+									height: '100%',
+									overflowY: 'scroll',
+								} }
+							>
+								<SensorsList
+									setSelectedDatastream={ setSelectedDatastream }
+									setDatastreamComparisonList={
+										setDatastreamComparisonList
+									}
+									selectedSensors={ selectedSensors }
+									setSelectedSensors={ setSelectedSensors }
 								/>
-							)}
+							</div>
+							<div style={ { width: '75%' } }>
+								{selectedDatastream && (
+									<DatastreamContent
+										datastream={ selectedDatastream }
+									/>
+								)}
+							</div>
 						</div>
-					</div>
-				) }
-			/>
+					) }
+				/>
 
-			<Route
-				path="/graphComparison"
-				render={ () => (
-					<ChartGraphComparison
-						dataStreams={ datastreamComparisonList }
-						setComparisonList={ setDatastreamComparisonList }
-					/>
-				) }
-			/>
+				<Route
+					path="/graphComparison"
+					element={ (
+						<ChartGraphComparison
+							dataStreams={ datastreamComparisonList }
+							setComparisonList={ setDatastreamComparisonList }
+						/>
+					) }
+				/>
 
-			<Route path="/sensor/:sensorName">
-				<SensorPage />
-			</Route>
+				<Route path="/sensor/:sensorName" element={ <SensorPage /> } />
+			</Routes>
 		</Router>
 	);
 }
