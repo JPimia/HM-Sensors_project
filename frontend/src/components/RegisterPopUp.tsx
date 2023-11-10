103e334cdbdfd1295d04e7ba27571bc781284363
masisoranummi <masi.soranummi@tuni.fi>
2023-11-10 17:33:08 +0200

    Add: Send login info to backend

--- frontend/src/components/RegisterPopUp.tsx
+++ frontend/src/components/RegisterPopUp.tsx
@@ -19,16 +19,35 @@ const RegisterComponent = () => {
 		setPassword(event.target.value);
 	};
 
-	const handleSubmit = (event: { preventDefault: () => void }) => {
+	const handleSubmit = async (event: { preventDefault: () => void }) => {
 		// TODO: Check backend if username is available
 		// TODO: Send username and pass to the backend
 		event.preventDefault();
 		if (username && password) {
 			const saltRounds = 10;
 			const hashedPassword = bcrypt.hashSync(password, saltRounds);
-			console.log('Username:', username);
-			console.log('Encrypted Password:', hashedPassword);
-			setCreated(true);
+			
+            try {
+                const response = await fetch('https://suzbt4f677.execute-api.eu-central-1.amazonaws.com/alpha/userInfoFunction', {
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ userName: username, passWord: password }),
+                    mode: 'cors',
+                });
+        
+                if (response.ok) {
+                    console.log(response);
+                    const jsonResponse = await response.json();
+                    console.log(jsonResponse);
+                    setCreated(true);
+                } else {
+                    console.error('Failed to register:', response.status, response.statusText);
+                }
+            } catch (error) {
+                console.error('Error registering user:', error);
+            }
 		}
 	};
 
