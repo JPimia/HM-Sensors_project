78400fc00577279b9429b875027e4996a9c976a5
Jari Pimiä <jari.pimia@tuni.fi>
2023-11-23 16:43:36 +0200

    Fix: modified buttons on filter options + added useNavigate for easier css

--- frontend/src/components/SensorsList.tsx
+++ frontend/src/components/SensorsList.tsx
@@ -2,65 +2,46 @@ import React, { useContext } from 'react';
 import { useState, useEffect } from 'react';
 import { fetchSensors, fetchDatastreamContents, fetchSensorNames } from './fetches';
 import '../CSS/SensorList.css';
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
 import DatePicker, { registerLocale, setDefaultLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import de from "date-fns/locale/de";
 import { SensorContext } from '../App';
+
 registerLocale("de", de);
 setDefaultLocale("de");
 
 export default function SensorsList() {
 	const { setSelectedSensors, setSelectedDatastream, setDatastreamComparisonList, selectedSensors } = useContext(SensorContext)!;
+	const [compareType, setCompareType] = useState(null);
+	const navigate = useNavigate(); // Use useNavigate instead of useHistory
 
-	const [compareType, setCompareType] = useState(null);//TODO: Move to app.tsx
-
-
-	async function handleCompareBtnClick(datastream: { [key: string]: any; }) {
-		const fullDatastream = await fetchDatastreamContents(
-			datastream['@iot.selfLink']
-		);
+	async function handleCompareBtnClick(datastream: { [key: string]: any }) {
+		const fullDatastream = await fetchDatastreamContents(datastream['@iot.selfLink']);
 		setCompareType(datastream.name);
-		setDatastreamComparisonList((prevList: any) => [
-			...prevList,
-			fullDatastream,
-		]);
+		setDatastreamComparisonList((prevList: any) => [...prevList, fullDatastream]);
 	}
-
 
 	async function getSensors(name?: string, timeframe?: string, locationName?: string) {
 		try {
-			const data = await fetchSensors(name, timeframe, locationName);
-			setSelectedSensors(data.value);
+		const data = await fetchSensors(name, timeframe, locationName);
+		setSelectedSensors(data.value);
 		} catch (error) {
-			console.error(error);
+		console.error(error);
 		}
 	}
 
-
-	async function getDatastream(datastream: { [key: string]: any; }) {
-
+	async function getDatastream(datastream: { [key: string]: any }) {
 		try {
-			const data = await fetchDatastreamContents(
-				datastream['@iot.selfLink']
-			);
-			setSelectedDatastream(data);
+		const data = await fetchDatastreamContents(datastream['@iot.selfLink']);
+		setSelectedDatastream(data);
 		} catch (error) {
-			console.error(error);
+		console.error(error);
 		}
 	}
 
-
-	function isCompareBtnDisabled(
-		datastreamType: string,
-		compareType: string | null
-	): boolean {
-		if (compareType === null) {
-			return false;
-		} else {
-			return datastreamType !== compareType;
-		}
+	function isCompareBtnDisabled(datastreamType: string, compareType: string | null): boolean {
+		return compareType !== null && datastreamType !== compareType;
 	}
 
 	function InputFields(): any {
@@ -107,62 +88,42 @@ export default function SensorsList() {
         }, []);
     
 		return (
-			<div className="input-container">
+			
+			<div className='input-container'>
 				<h4>Filter options:</h4>
-				<span>Sensor Name</span>
-				<p>
-					Search for sensors using name as filter, leave empty to show
-					all.
-				</p>
-                <button
-					onClick={() =>
-						getSensors(
-							sensorNameRef.current?.value,
-							timeframeRef.current?.value,
-							locationNameRef.current?.value
-						)
-					}
-					className="buttons"
-					style={{
-						width: "100%",
-						marginTop: "10px",
-						marginBottom: "10px",
-					}}
-				>
-					Search for sensors
-				</button>
-                <div className="dropdown-container">
-                    <input
-                        type="text"
-                        ref={sensorNameRef}
-                        value={userInput}
-                        onChange={handleInputChange}
-                        placeholder="Type to search..."
-                    />
-                    <ul className="dropdown-list">
-                        {
-                            // Currently only shows the first 10 items in the suggestion array
-                        }
-                        {suggestions.slice(0, 10).map((suggestion, index) => (
-                            <li
-                                key={index}
-                                onClick={() => handleSuggestionClick(suggestion)}
-                            >
-                                {suggestion}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                {
-                    /*
-                        original search input field
-                        <input
-                            type="text"
-                            ref={sensorNameRef}
-                            defaultValue="hm sensor"
-                        />
-                    */
-                }
+				<p>Search for sensors using name as filter, leave empty to show all.</p>
+				<div className="dropdown-container">
+					<input
+						type="text"
+						ref={sensorNameRef}
+						value={userInput}
+						onChange={handleInputChange}
+						placeholder="Type to search..."
+					/>
+					<ul className="dropdown-list">
+						{
+							// Currently only shows the first 10 items in the suggestion array
+						}
+						{suggestions.slice(0, 10).map((suggestion, index) => (
+							<li
+								key={index}
+								onClick={() => handleSuggestionClick(suggestion)}
+							>
+								{suggestion}
+							</li>
+						))}
+					</ul>
+				</div>
+				{
+					/*
+						original search input field
+						<input
+							type="text"
+							ref={sensorNameRef}
+							defaultValue="hm sensor"
+						/>
+					*/
+				}
 
 
 				{/* 				<span>Location Name</span>
@@ -184,69 +145,60 @@ export default function SensorsList() {
 						/>
 					</div>
 				</div> */}
-				<Link to="/graphComparison">
-					<button
-						className="buttons"
-						style={{ backgroundColor: "green", width: "100%" }}
-					>
+				{/* <input
+					type="text"
+					ref={sensorNameRef}
+					defaultValue="hm sensor"
+				/> */}
+				<div className='input-container-buttons'>
+					<button onClick={() => getSensors(sensorNameRef.current?.value, timeframeRef.current?.value, locationNameRef.current?.value)} className='input-container-red-button' style={{ marginTop: "10px", marginBottom: "10px" }}>
+						Fetch Sensors
+					</button>
+					<button onClick={() => navigate('/graphComparison')} className='input-container-green-button'>
 						Graph Comparison
 					</button>
-				</Link>
+				</div>
 			</div>
 		);
-
 	}
+
 	return (
 		<div>
-			<div>
-				<InputFields />
-			</div>
-			
-			{selectedSensors ? ( // Check if filter returned any results
-				<div className='sensors-container'>
-					{selectedSensors.map((sensor: any) => (
-						<div className="sensor-container" key={sensor.name}>
-							<div key={sensor.name}>
-								<div className='sensor-info'>
-									<h3 className='sensor-name'>{sensor.name}</h3>
-									<span style={{ marginBottom: '5px' }}>Sensor ID: {sensor['@iot.id']}</span>
-									<span style={{ marginBottom: '5px' }}>Description: {sensor.description}</span>
-								</div>
-								{sensor.Datastreams.map((datastream: any) => (
-									<div key={datastream.name} className='button-container'>
-										<button
-											onClick={() => getDatastream(datastream)}
-											className='red-button'
-										>
-											{datastream.name}
-										</button>
-										<button
-											onClick={() => handleCompareBtnClick(datastream)}
-											className='green-button'
-											style={{
-												backgroundColor: isCompareBtnDisabled(
-													datastream.name,
-													compareType
-												)
-													? 'gray'
-													: 'green',
-											}}
-											disabled={isCompareBtnDisabled(
-												datastream.name,
-												compareType
-											)}
-										>
-											Add to compare
-										</button>
-									</div>
-								))}
-							</div>
-						</div>
+		<InputFields />
+		{selectedSensors ? (
+			<div className='sensors-container'>
+			{selectedSensors.map((sensor: any) => (
+				<div className="sensor-container" key={sensor.name}>
+				<div key={sensor.name}>
+					<div className='sensor-info'>
+					<h3 className='sensor-name'>{sensor.name}</h3>
+					<span style={{ marginBottom: '5px' }}>Sensor ID: {sensor['@iot.id']}</span>
+					<span style={{ marginBottom: '5px' }}>Description: {sensor.description}</span>
+					</div>
+					{sensor.Datastreams.map((datastream: any) => (
+					<div key={datastream.name} className='button-container'>
+						<button onClick={() => getDatastream(datastream)} className='red-button'>
+						{datastream.name}
+						</button>
+						<button
+						onClick={() => handleCompareBtnClick(datastream)}
+						className='green-button'
+						style={{
+							backgroundColor: isCompareBtnDisabled(datastream.name, compareType) ? 'gray' : 'green',
+						}}
+						disabled={isCompareBtnDisabled(datastream.name, compareType)}
+						>
+						Add to compare
+						</button>
+					</div>
 					))}
 				</div>
-			) : (
-				<p>No sensors found</p>
-			)}
+				</div>
+			))}
+			</div>
+		) : (
+			<p>No sensors found</p>
+		)}
 		</div>
 	);
 }
