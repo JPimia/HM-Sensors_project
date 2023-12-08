4ec67f82501418e3bf1aa9e55b9c34a621ec5d99
masisoranummi <masi.soranummi@tuni.fi>
2023-12-08 08:18:36 +0200

    Add: User manual download  button

--- frontend/src/components/HomeComponent.tsx
+++ frontend/src/components/HomeComponent.tsx
@@ -1,7 +1,6 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext} from 'react';
 import { DatastreamContent } from './DatastreamContents';
 import SensorsList from './SensorsList';
-import PopupComponent from './PopupComponent';
 import { SensorContext } from '../App';
 import '../CSS/LandingPage.css';
 
@@ -11,78 +10,99 @@ export function HomeComponent() {
         selectedDatastream
     } = useContext(SensorContext)!;
 
+    const onButtonClick = () => {
+		const pdfUrl = "sample.pdf";
+		const link = document.createElement("a");
+		link.href = pdfUrl;
+		link.download = "sample.pdf"; 
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	};
 
     return (
 		<div className="main-container">
-			<div style={{ display: "flex" }}>
-				{
-					//<GitLabAuth setUser={setUser}/>
-				}
-				<PopupComponent />
-				{/* <Link
-                to="/register"
-                style={ { backgroundColor: 'yellow' } }
-            >
-                Register
-            </Link> */}
+			<SensorsList />
+			<div
+				style={{
+					overflowY: "scroll",
+					overflowX: "hidden",
+					width: "100%",
+					paddingRight: "10px",
+				}}
+			>
+				{selectedDatastream ? (
+					<DatastreamContent />
+				) : (
+					<div
+						className="landing-text"
+						style={{ marginLeft: "10px" }}
+					>
+						<h1>
+							<center>Welcome to HM InsightHub!</center>
+						</h1>
+						<br />
+						<br />
+						<p>
+							<strong>
+								Welcome to HM InsightHub: Empowering Your Campus
+								with Data-Driven Insights.
+							</strong>{" "}
+							<br /> Unlock the power of data with HM InsightHub,
+							your centralized access point for customizing and
+							exporting real-time sensor data from across the
+							university. <br /> Whether you're an academic or
+							student, the tools for applied research and
+							sustainability at your fingertips.
+						</p>
+						<p>
+							Enhance research and course content with real-world
+							data, fostering a data-driven academic environment.
+						</p>{" "}
+						<br />
+						<h4>How to get started:</h4> <br />
+						<p>
+							1.{" "}
+							<strong>
+								Fetch a list of available sensors on the left.
+							</strong>{" "}
+							<br />
+							&nbsp;&nbsp;&nbsp; In the search bar, type in “hm
+							sensor”, select a faculty location (if applicable)
+							and click “Search”.
+						</p>
+						<p>
+							2.{" "}
+							<strong>
+								Select the variables you would like to download
+								and customize your dataset.
+							</strong>{" "}
+							<br />
+							&nbsp;&nbsp;&nbsp; Click the ”Download” button in
+							the ribbon on the top of the page and further filter
+							your data there if you need to.
+						</p>
+						<p>
+							3.{" "}
+							<strong>
+								Review the output table and download your file.
+							</strong>{" "}
+							<br />
+							&nbsp;&nbsp;&nbsp; Select your desired file format
+							(csv or JSON) and download the file.
+						</p>{" "}
+						<br />
+						<p>
+							<strong>
+								Ready to Explore? Dive into the Data Now!
+							</strong>
+						</p>
+						<button onClick={onButtonClick}>
+							Download user manual
+						</button>
+					</div>
+				)}
 			</div>
-			<div style={{ display: "flex", marginBottom: "0px" }}>
-				{/* <Link to="/">Home view</Link>
-            <Link
-                to="/graphComparison"
-                style={ { backgroundColor: 'green' } }
-            >
-                Graph Comparison
-            </Link> */}
-			</div>
-				<SensorsList />
-				<div style={{ overflowY: "scroll", overflowX: "hidden", width: "100%", paddingRight: "10px" }}>
-					{selectedDatastream ? (
-						<DatastreamContent />
-					) : (
-						<div className='landing-text' style={{marginLeft: "10px"}}>
-							<h1><center>Welcome to HM InsightHub!</center></h1><br/><br/>
-							<p>
-								<strong>Welcome to HM InsightHub: Empowering Your Campus with Data-Driven Insights.</strong> <br/> Unlock the power of
-								data with HM InsightHub, your centralized access
-								point for customizing and exporting real-time
-								sensor data from across the university. <br/> Whether
-								you're an academic or student, the tools for
-								applied research and sustainability at your
-								fingertips.
-							</p>
-							<p>
-								Enhance research and course content with
-								real-world data, fostering a data-driven
-								academic environment.
-							</p> <br/>
-							
-							<h4>How to get started:</h4> <br/>
-							
-								<p>
-								1.  <strong>Fetch a list of available sensors on the
-									left.</strong> <br/>&nbsp;&nbsp;&nbsp; In the search bar, type in “hm sensor”, select a faculty location (if applicable) and click “Search”.
-								</p>
-								<p>
-								2.  <strong>Select the variables
-									you would like to download and customize your
-									dataset.</strong> <br/>&nbsp;&nbsp;&nbsp; Click the ”Download” button in the
-									ribbon on the top of the page and further filter
-									your data there if you need to.
-								</p>
-								<p>
-								3.  <strong>Review the
-									output table and download your file.</strong> <br/>&nbsp;&nbsp;&nbsp; Select your
-									desired file format (csv or JSON) and download
-									the file.
-								</p> <br/>
-								<p><strong>Ready to Explore? Dive into the Data Now!</strong></p>
-							
-                            
-						</div>
-					)}
-				</div>
-			
 		</div>
 	);
 }
