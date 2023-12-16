da891498860dcde8bb83a0a80e2c9aed38cd6ce5
Koydenjatke <97977463+Koydenjatke@users.noreply.github.com>
2023-12-16 23:48:06 +0200

    Add counter for download page, add blinker styles.

--- src/components/navbarContainer.tsx
+++ src/components/navbarContainer.tsx
@@ -1,16 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../images/SensorLogo.png';
 import { SensorContext } from '../App';
 import GitLabAuth from './GitLabAuth';
 
 export function NavbarContainer(): JSX.Element {
 	const {
-		datastreamComparisonList
+		datastreamComparisonList,
+		selectedSensors,
 	} = useContext(SensorContext)!;
+
+	const [blinkGraphComparison, setBlinkGraphComparison] = useState(false);
+	const [blinkDownload, setBlinkDownload] = useState(false);
+
+	useEffect(() => {
+		setBlinkGraphComparison(true);
+		setTimeout(() => setBlinkGraphComparison(false), 2000);
+	}, [datastreamComparisonList.length]);
+
+	useEffect(() => {
+		setBlinkDownload(true);
+		setTimeout(() => setBlinkDownload(false), 2000);
+	}, [selectedSensors.length]);
 
 	return (
 		<div>
@@ -25,13 +38,13 @@ export function NavbarContainer(): JSX.Element {
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="mr-auto">
 						<Nav.Link as={Link} to="/" className="buttons">Sensor explorer</Nav.Link>
-						<Nav.Link as={Link} to="/graphComparison" className="buttons">Graph Comparison {datastreamComparisonList.length}</Nav.Link>
-						<Nav.Link as={Link} to="/selectedDataset" className="buttons">Download</Nav.Link>
+						<Nav.Link as={Link} to="/graphComparison" className={`buttons ${blinkGraphComparison ? 'blink' : ''}`}>Graph Comparison {datastreamComparisonList.length}</Nav.Link>
+						<Nav.Link as={Link} to="/selectedDataset" className={`buttons ${blinkDownload ? 'blink' : ''}`}>Download {selectedSensors.length}</Nav.Link>
 						<Nav.Link as={Link} to="/HmBatteries" className="buttons">Quick: HM Sensor batteries</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 				{
-                    // Only show login option if the user has added the enviromental values
+					// Only show login option if the user has added the enviromental values
 					process.env.REACT_APP_GITLAB_CLIENT_ID ? (
 						<div>
 							<GitLabAuth />
